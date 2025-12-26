225b7f1c1c17b8798b757dce5eb98b96b28a68e3
podoviske <natanaelitalo10@gmail.com>
2025-12-26 05:59:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -414,7 +414,7 @@ if check_password():
                         prob_liquidez = ((1 - win_rate_dec) ** vidas_u) * 100
                         prob_liquidez = min(max(prob_liquidez, 0.0), 100.0)
                         
-                        display_ror = f"{prob_liquidez:.9f}%"
+                        display_ror = f"{prob_liquidez:.8f}%"
                         
                         if prob_liquidez < 10: color_r = "#00FF88"
                         elif prob_liquidez < 40: color_r = "#FFFF00"
